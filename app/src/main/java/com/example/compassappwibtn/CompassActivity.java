5f03fd2d98df2c886ff1429eef5cbf5515c559b1
Hyejeong00 <love72771112@gmail.com>
2021-04-20 13:34:45 +0900

    compass activity using button + sensors

--- app/src/main/java/com/example/compassappwibtn/CompassActivity.java
+++ app/src/main/java/com/example/compassappwibtn/CompassActivity.java
@@ -1,14 +1,63 @@
 package com.example.compassappwibtn;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
 public class CompassActivity extends Activity {
+    CompassView compassView;
+    SensorManager sensorManager;
+    SensorEventListener listener;
+    Sensor magneticSensor, accelSensor;
+    float []  magneticValues, accelValues;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+
+        compassView = findViewById(R.id.compassView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                switch (sensorEvent.sensor.getType()) {
+                    case Sensor.TYPE_ACCELEROMETER:
+                        accelValues = sensorEvent.values.clone();
+                        break;
+                    case Sensor.TYPE_MAGNETIC_FIELD:
+                        magneticValues = sensorEvent.values.clone();
+                        break;
+                    default:
+                        break;
+                }
+                if (magneticValues != null && accelValues != null) {
+                    float[] R = new float[16];
+                    float[] I = new float[16];
+                    SensorManager.getRotationMatrix(R, I, accelValues, magneticValues);
+                    float[] values = new float[3];
+                    sensorManager.getOrientation(R, values);
+
+                    compassView.azimuth = (int) radiant2degree(values[0]);
+                    compassView.invalidate();
+                }
+            }
+            
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) { }
+        };
+        sensorManager.registerListener(listener, magneticSensor, sensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(listener, accelSensor, sensorManager.SENSOR_DELAY_UI);
+
     }
+
+    private float radiant2degree(float radian) {return  radian * 180 / (float)Math.PI;}
 }
